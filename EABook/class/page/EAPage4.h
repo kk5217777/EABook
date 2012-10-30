525cc0540dca76ba0967fe9b8a5c5b6d1c6ee432
ChristineLo <yatinglo@yahoo.com.tw>
2012-10-30 19:23:45 +0800

    基本動畫
    3-2, 3-3還沒

---
+++ EABook/class/page/EAPage4.h
@@ -0,0 +1,16 @@
+//
+//  EAPage4.h
+//  EABook
+//
+//  Created by Mac06 on 12/10/30.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface EAPage4 : CCLayer {
+    
+}
+
+@end
---
+++ EABook/class/page/EAPage4.m
@@ -0,0 +1,14 @@
+//
+//  EAPage4.m
+//  EABook
+//
+//  Created by Mac06 on 12/10/30.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "EAPage4.h"
+
+
+@implementation EAPage4
+
+@end
